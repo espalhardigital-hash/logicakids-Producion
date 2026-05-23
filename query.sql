831393836221abf95f786f09546fd1be1b748a83
amilcar <amilcar.najul@gmail.com>
2026-05-23 11:59:18 -0300

    eliminar archivos obsoletos

--- query.sql
+++
@@ -1,5 +0,0 @@
-SELECT t.table_name, c.column_name, c.data_type, c.is_nullable 
-FROM information_schema.tables t 
-JOIN information_schema.columns c ON t.table_name = c.table_name 
-WHERE t.table_schema = 'public' AND t.table_type = 'BASE TABLE' 
-ORDER BY t.table_name, c.ordinal_position;
